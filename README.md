a3147be8ed084bf1311b08dc0a50aa8cc05fe6fe
ssnseawolf <integratebyparts@gmail.com>
2020-12-23 15:49:48 -0600

    Filled out install

---
+++ README.md
@@ -0,0 +1,11 @@
+# easytime
+
+A simple script to get a full-blown NTS timeserver
+
+This script is designed to be simple, basic and straightforward. It is not designed to be used in a variety of environments, only a fresh CentOS/RHEL install or very similar.
+
+
+1. Download and install a fresh copy of CentOS/RHEL, preferably on a virtual machine as root.
+2. ```bash <(curl -s https://raw.githubusercontent.com/ssnseawolf/easytime/master/easytime.sh)```
+
+Machine will reboot with a timeserver for your network.
--- easytime.sh
+++ easytime.sh
@@ -5,23 +5,30 @@ SERVER_IP_ADDR=""        # IP address to use for your server. Typically 192.168.
 SERVER_IP_NETMASK_CIDR=""
 GATEWAY_IP_ADDR=""
 DOMAIN=""
-TIME_ZONE=""
+TIME_ZONE="" # Formatted as 'Continent/City'
 
 # Request default IP
 read -p "Static IP to set [192.168.1.10]: " SERVER_IP_ADDR
 read -p "CIDR subnet. No slash, i.e '16' or '24'. [24]: " SERVER_IP_NETMASK_CIDR
 read -p "Gateway IP [192.168.1.1]: " GATEWAY_IP_ADDR
 read -p "Domain, e.x. example.com [no domain]: " DOMAIN
-read -p "Time zone [Eastern]: " TIME_ZONE
+read -p "Time zone [America/New York]: " TIME_ZONE
 SERVER_IP_ADDR=${SERVER_IP_ADDR:-192.168.1.10}
 SERVER_IP_NETMASK_CIDR=${SERVER_IP_NETMASK_CIDR:-24}
 GATEWAY_IP_ADDR=${GATEWAY_IP_ADDR:-192.168.1.1}
-TIME_ZONE=${TIME_ZONE:-Eastern}
+TIME_ZONE=${TIME_ZONE:-"America/New York"}
 
 # Make sure server is updated and needed packages are installed
 dnf -y upgrade
 dnf install -y ntsec
 dnf install -y dnf-automatic
+
+# Configure our timeserver (chrony)
+CHRONY_CONF=https://raw.githubusercontent.com/ssnseawolf/easytime/master/chrony.conf
+curl $CHRONY_CONF | tee /etc/chrony.conf > /dev/null
+
+# Set the server's time zone
+timedatectl set-timezone $Time_Zone
 
 # Punch a hole through the firewall for NTPsec
 # firewall-cmd --add-service=dns --permanent
@@ -35,7 +42,7 @@ nmcli connection modify eth0 IPv4.address $SERVER_IP_ADDR/$SERVER_IP_NETMASK_CID
 nmcli connection modify eth0 IPv4.gateway $GATEWAY_IP_ADDR
 nmcli connection modify eth0 IPv4.method manual
 
-# Disable ssh
+# Disable SSH
 systemctl disable sshd
 
 # We lazy
