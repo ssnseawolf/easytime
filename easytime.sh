ea1e581a3c7bc8600473f0ba69ddb96d45ef963c
ssnseawolf <integratebyparts@gmail.com>
2020-12-26 10:57:39 -0600

    Change time zone to always be UTC

--- easytime.sh
+++ easytime.sh
@@ -5,18 +5,15 @@ SERVER_IP_ADDR=""        # IP address to use for your server. Typically 192.168.
 SERVER_IP_NETMASK_CIDR=""
 GATEWAY_IP_ADDR=""
 DOMAIN=""
-TIME_ZONE="" # Formatted as 'Continent/City'
 
 # Request default IP
 read -p "Static IP to set [192.168.1.10]: " SERVER_IP_ADDR
 read -p "CIDR subnet. No slash, i.e '16' or '24'. [24]: " SERVER_IP_NETMASK_CIDR
 read -p "Gateway IP [192.168.1.1]: " GATEWAY_IP_ADDR
 read -p "Domain, e.x. example.com [no domain]: " DOMAIN
-read -p "Time zone [America/New York]: " TIME_ZONE
 SERVER_IP_ADDR=${SERVER_IP_ADDR:-192.168.1.10}
 SERVER_IP_NETMASK_CIDR=${SERVER_IP_NETMASK_CIDR:-24}
 GATEWAY_IP_ADDR=${GATEWAY_IP_ADDR:-192.168.1.1}
-TIME_ZONE=${TIME_ZONE:-"America/New York"}
 
 # Make sure server is updated and needed packages are installed
 dnf -y upgrade
@@ -27,8 +24,12 @@ dnf install -y dnf-automatic
 CHRONY_CONF=https://raw.githubusercontent.com/ssnseawolf/easytime/master/chrony.conf
 curl $CHRONY_CONF | tee /etc/chrony.conf > /dev/null
 
-# Set the server's time zone
-timedatectl set-timezone $TIME_ZONE
+# Set the server's time zone to UTC
+# WARNING: DON'T CHANGE, UTC TIME IS IMPORTANT TO AVOID WEIRD DAYLIGHT SAVINGS ERRORS
+timedatectl set-timezone UTC
+
+# Create chrony-specific files
+touch /var/lib/chrony/rtc # Real-time clock file
 
 # Punch a hole through the firewall for NTPsec
 # firewall-cmd --add-service=dns --permanent
@@ -41,6 +42,9 @@ timedatectl set-timezone $TIME_ZONE
 nmcli connection modify eth0 IPv4.address $SERVER_IP_ADDR/$SERVER_IP_NETMASK_CIDR
 nmcli connection modify eth0 IPv4.gateway $GATEWAY_IP_ADDR
 nmcli connection modify eth0 IPv4.method manual
+
+# Log out after an hour
+echo "TIMEOUT=3600" | tee ~/.bashrc
 
 # Disable SSH
 systemctl disable sshd
